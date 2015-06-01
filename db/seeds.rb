d97a7506b230d2e7ad9bf3b7b35d999bcf7668c3
guillermo_salazar <guillermos_2007@yahoo.com>
2015-06-01 16:29:57 +0000

    Added validations to Product and update test to pass

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,24 +17,28 @@ Product.create!(
 Product.create!(
     title: 'Paper',
     description: 'College-ruled',
+    image_url: 'http://images.all-free-download.com/images/graphiclarge/nexxuz_loose_leaf_paper_clip_art_23285.jpg',
     price: 0.20
 )
 
 Product.create!(
     title: 'Tablet',
     description: 'A tablet',
+    image_url: 'http://sr.photos3.fotosearch.com/bthumb/CSP/CSP852/k8527509.jpg',
     price: 499.99
 )
 
 Product.create!(
     title: 'Eraser',
     description: 'Pink eraser',
+    image_url: 'http://clipartzebraz.com//cliparts/eraser-clip-art/cliparti1_eraser-clip-art_06.jpg',
     price: 0.75
 )
 
 Product.create!(
     title: 'Highlighter',
     description: 'A black highlighter',
+    image_url: 'http://www.clker.com/cliparts/C/V/s/C/O/8/yellow-highlighter-marker-hi.png',
     price: 1.89
 )
     
